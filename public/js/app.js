012729b8c4c4308f49dc57e192d30834ac1bd9c5
habib <habib@thebosstech.com>
2021-01-19 12:27:02 -0800

    ic

--- public/js/app.js
+++ public/js/app.js
@@ -56055,8 +56055,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\demo1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\demo1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Dell\Desktop\Demo\component-based-demo-in-vuejs\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Dell\Desktop\Demo\component-based-demo-in-vuejs\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
